1a943b58acb16e56b143d2f1559e38117c797c1d
Win Nguyen <winnguyen1905.work@gmail.com>
2024-12-04 18:09:01 +0700

    init gateway service

---
+++ src/main/java/com/winnguyen1905/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.winnguyen1905.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winnguyen1905/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winnguyen1905.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
