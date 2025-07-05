ff0bd7d47c81a8a4647e396fda929ba734cf1446
Win Nguyen <winnguyen1905.work@gmail.com>
2025-07-05 22:14:32 +0700

    Add spring-boot-starter-test dependency, create GatewayServiceApplication class, and update application.yaml for Eureka and gateway configuration

--- src/main/java/com/winnguyen1905/gateway/GatewayServiceApplication.java
+++ src/main/java/com/winnguyen1905/gateway/GatewayServiceApplication.java
@@ -1,11 +1,10 @@
-package com.winnguyen1905.auth;
+package com.winnguyen1905.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
